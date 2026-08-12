f2d3912198ae4c0304ea10e0cef738df12720d13
Isiri <isiri.lanka@gmail.com>
2026-08-13 03:47:36 +0530

    feat: complete Electron React application scaffold

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(async () => {
+  const { default: react } = await import('@vitejs/plugin-react');
+
+  return {
+    plugins: [react()],
+  };
+});
